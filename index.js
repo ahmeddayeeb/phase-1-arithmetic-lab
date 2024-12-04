3395115bcdafe8f2d4c44b15abd223d8e1cb5e72
ahmed <ahmeddayeeb@gmail.com>
2024-12-04 07:18:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Challenge 1: Create a variable called multiply that multiplies num1 and num2 to equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Challenge 2: Create a variable called random that generates a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Challenge 3: Create a variable called mod that calculates the remainder of num3 divided by num4, resulting in 4
+let num3 = 28;
+let num4 = 12;
+let mod = num3 % num4;
+
+// Challenge 4: Create a variable called max that finds the highest number in a set, returning 20
+let max = Math.max(5, 10, 15, 20);
